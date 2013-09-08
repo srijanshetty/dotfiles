faac8064b20bfff6c52a24144ae4ddd9883976a2
Srijan R Shetty <srijan.shetty@gmail.com>
2013-09-08 16:24:05 +0530

    Better structure for vim, ack in custom.sh

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 function install_ack {
-    if [[ -d ${HOME}/Documents/GitHub ]]; then
+    if [[ -d ${HOME}/Documents/local/bin ]]; then
         echo "The home directory already exists"
     else
-        mkdir -p ${HOME}/GitHub
+        mkdir -p ${HOME}/local/bin
     fi
-    wget -O "${HOME}/Documents/GitHub/ack" http://beyondgrep.com/ack-2.08-single-file 
-    chmod u+x "${HOME}/Documents/GitHub/ack"
+    wget -O "${HOME}/Documents/local/bin/ack" http://beyondgrep.com/ack-2.08-single-file 
+    chmod u+x "${HOME}/Documents/local/bin/ack"
 }
 
 while [[ -n "$1" ]]; do
