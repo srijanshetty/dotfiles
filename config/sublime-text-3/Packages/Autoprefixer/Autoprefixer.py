91ed3be1c01b029e832bf602a293109a9f5f7652
Srijan R Shetty <srijan.shetty@gmail.com>
2013-10-08 12:45:52 +0530

    The files weren't added sublime-text-3 updates

--- config/sublime-text-3/Packages/Autoprefixer/Autoprefixer.py
+++ config/sublime-text-3/Packages/Autoprefixer/Autoprefixer.py
@@ -1,23 +1,22 @@
 import sublime
 import sublime_plugin
-import os
-import platform
-from subprocess import Popen, PIPE
 from os.path import dirname, realpath, join
 
+try:
+	# Python 2
+	from node_bridge import node_bridge
+except:
+	from .node_bridge import node_bridge
 
 # monkeypatch `Region` to be iterable
 sublime.Region.totuple = lambda self: (self.a, self.b)
 sublime.Region.__iter__ = lambda self: self.totuple().__iter__()
 
-BIN_PATH = join(sublime.packages_path(), dirname(realpath(__file__)), 'autoprefixer', 'bin', 'autoprefixer')
-IS_WINDOWS = platform.system() == 'Windows'
-IS_OSX = platform.system() == 'Darwin'
-
+BIN_PATH = join(sublime.packages_path(), dirname(realpath(__file__)), 'autoprefixer.js')
 
 class AutoprefixerCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		if not self.is_css():
+		if not self.is_css() and not self.is_unsaved_buffer_without_syntax():
 			return
 		self.browsers = ','.join(sublime.load_settings('Autoprefixer.sublime-settings').get('browsers'))
 		if not self.has_selection():
@@ -36,25 +35,10 @@ class AutoprefixerCommand(sublime_plugin.TextCommand):
 				self.view.replace(edit, region, prefixed)
 
 	def prefix(self, data):
-		env = None
-		if IS_OSX:
-			# GUI apps in OS X doesn't contain .bashrc set paths
-			env = os.environ.copy()
-			env['PATH'] += ':/usr/local/bin'
 		try:
-			p = Popen(['node', BIN_PATH, '-b', self.browsers],
-				stdout=PIPE, stdin=PIPE, stderr=PIPE,
-				env=env, shell=IS_WINDOWS)
-		except OSError:
-			sublime.error_message('Couldn\'t find Node.js. Make sure it\'s in your $PATH. See install guide: https://github.com/sindresorhus/sublime-autoprefixer')
-			return
-		stdout, stderr = p.communicate(input=data.encode('utf-8'))
-		stdout = stdout.decode('utf-8')
-		stderr = stderr.decode('utf-8')
-		if stderr:
-			sublime.error_message('Autoprefixer error: ' + stderr)
-		else:
-			return stdout
+			return node_bridge(data, BIN_PATH, [self.browsers])
+		except Exception as e:
+			sublime.error_message('Autoprefixer\n%s' % e)
 
 	def has_selection(self):
 		for sel in self.view.sel():
@@ -62,6 +46,12 @@ class AutoprefixerCommand(sublime_plugin.TextCommand):
 			if start != end:
 				return True
 		return False
+
+	def is_unsaved_buffer_without_syntax(self):
+		return self.view.file_name() == None and self.is_plaintext() == True
+
+	def is_plaintext(self):
+		return self.view.settings().get('syntax') == 'Packages/Text/Plain text.tmLanguage'
 
 	def is_css(self):
 		return self.view.settings().get('syntax') == 'Packages/CSS/CSS.tmLanguage'
