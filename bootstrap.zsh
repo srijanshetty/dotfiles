de6a910d4d2d985a02f9503c9af7b022b40530ce
Srijan R Shetty <srijan.shetty@gmail.com>
2014-07-26 12:15:35 +0530

    Refactored Bootstrap

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -35,67 +35,86 @@ _EOH_
 # Configuration Functions
 function config_node() {
     if hash nvm &> /dev/null;then
-        nvm install v0.10.28 && nvm use v0.10.28
-        success "NVM installed"
+        if nvm install v0.10.28 && nvm use v0.10.28; then
+            success "NVM : configured"
+            return 0
+        else
+            fail "NVM : Error with NVM script"
+            return 1
+        fi
     else
-        fail "NVM is not installed"
+        fail "NVM : nvm not installed"
+        return 1
     fi
 }
 
 # Vim configuration
 function config_vim() {
     highlight "\nConfiguring Vim"
+
     if hash vim &> /dev/null; then
-        cd
-        if [ -d .vim ]; then
-            fail "Vim configuration failed. Delete ~/.vim and retry"
-            ERR=1
+        if [ -d ~/.vim ]; then
+            fail "Vim : delete ~/.vim and retry"
+            return 1
         else
-            ln -s "${CONFDIR}/config/vim/vim" .vim;
-            ln -s "${CONFDIR}/config/vim/vimrc" .vimrc
-            success "Vim configuration successful"
+            if ln -s "${CONFDIR}/config/vim/vim" ~/.vim && ln -s "${CONFDIR}/config/vim/vimrc" ~/.vimrc; then
+                success "Vim : configured"
+                return 0
+            else
+                fail "Vim : Symbolic link creation failed"
+                return 1
+            fi
         fi
-        cd $CONFDIR
     else
-        fail "Vim configuration failed. Install vim first"
-        ERR=2
+        fail "Vim : Vim not installed"
+        return 1
     fi
 }
 
 # Git configuration
 function config_git() {
     highlight "\nConfiguring git"
+
     if hash git &> /dev/null; then
-        cd
         # Configure git
-        if [ -e .gitconfig ]; then
-            fail "Git configuration exists. Delete ~./gitconfig and retry"
+        if [ -e ~/.gitconfig ]; then
+            fail "Git : delete ~./gitconfig and retry"
+            return 1
         else
-            ln -s "${CONFDIR}/config/git/gitconfig" .gitconfig
-            success "Git configured."
+            if ln -s "${CONFDIR}/config/git/gitconfig" ~/.gitconfig; then
+                success "Git : configured."
+            else
+                fail "Git : Symbolic link creation failed for git"
+                return 0
+            fi
         fi
 
         # Configure global gitignore
-        if [ -e .gitignore_global ]; then
-            fail "Git ignore exists. Delete ~./gitignore_global and retry"
+        if [ -e ~/.gitignore_global ]; then
+            fail "Git : ~/.gitignore_global and retry"
         else
-            ln -s "${CONFDIR}/config/git/gitignore_global" .gitignore_global
-            success "Global gitignore configured"
+            if ln -s "${CONFDIR}/config/git/gitignore_global" ~/.gitignore_global; then
+                success "Global gitignore configured"
+                return 0
+            else
+                fail "Git : Symbolic link for ~/.gitignore_global failed"
+                return 1
+            fi
         fi
     else
-        fail "Git configuration failed. Install git first"
-        ERR=2
+        fail "Git : install git"
+        return 1
     fi
 }
 
 #ZSH configuration
 function config_zsh() {
     highlight "\nConfiguring zsh"
+
     if hash zsh &> /dev/null; then
-        cd
-        if [[ -d .zprezto ]]; then
-            fail "Prezto configuration failed. Delete ~/.zprezto and retry"
-            ERR=1
+        if [[ -d ~/.zprezto ]]; then
+            fail "Prezto : delete ~/.zprezto and retry"
+            return 1
         else
             # change default shell to zsh
             if [ -n $ZSH_NAME ]; then
@@ -104,105 +123,136 @@ function config_zsh() {
                 chsh -s /bin/zsh
             fi
 
-            ln -s "${CONFDIR}/shells/zsh/zprezto" .zprezto
+            ln -s "${CONFDIR}/shells/zsh/zprezto" ~/.zprezto
             setopt EXTENDED_GLOB
             for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
                 ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
             done
-            cd ${CONFDIR}
-            success "Prezto configured."
+
+            success "Prezto : configured."
+            return 0
         fi
     else
-        fail "Zsh configuration failed. Install zsh first"
-        ERR=2
+        fail "Zsh : install zsh"
+        return 1
     fi
 }
 
 #Configuration file for tmux
 function config_tmux() {
     highlight "\nConfiguring tmux"
+
     if hash tmux; then
-        cd
-        if [ -e .tmux.conf ]; then
-            fail "tmux configuration failed. Delete ~/.tmux.conf and retry"
-            ERR=1
+        if [ -e ~/.tmux.conf ]; then
+            fail "tmux : delete ~/.tmux.conf and retry"
+            return 1
         else
-            ln -s "${CONFDIR}/config/tmux.conf" .tmux.conf
-            cd ${CONFDIR}
-            success "tmux configured"
+            if ln -s "${CONFDIR}/config/tmux.conf" .tmux.conf; then
+                success "tmux : configured"
+                return 0
+            else
+                fail "tmux : Symbolic link failure"
+                return 1
+            fi
         fi
     else
-        fail "tmux configuration failed. Install tmux first"
-        ERR=2
+        fail "tmux : install tmux"
+        return 1
     fi
 }
 
 # Configure music
 function config_music() {
     highlight "\nConfiguring Beets"
+
     if hash beet &> /dev/null; then
-        cd
-        if [ -d .config/beets ]; then
-            fail "Beets configuration failed. Delete ~/.config/beets and retry"
-            ERR=1
+        if [ -d ~/.config/beets ]; then
+            fail "Beets : delete ~/.config/beets and retry"
+            return 1
         else
-            ln -s "${CONFDIR}/config/beets" .config/beets
-            cd ${CONFDIR}
-            success "Beets configured"
+            if ln -s "${CONFDIR}/config/beets" ~/.config/beets;then
+                success "Beets : configured"
+                return 0
+            else
+                fail "Beets : symbolic link failure"
+                return 1
+            fi
         fi
     else
-        fail "beets configuration failed. Install beet first"
-        ERR=2
+        fail "beets : install beet"
+        return 1
     fi
 }
 
 #configure writing tools
 function config_write() {
     highlight "Configuring LaTeX"
-    if hash pdflatex; then
-        cd
-        if [ -d texmf ]; then
-            fail "Texmf already exists. Delete ~/texmf and retry"
-            ERR=1
+
+    if hash pdflatex &> /dev/null; then
+        if [ -d ~/texmf ]; then
+            fail "LaTex : Delete ~/texmf and retry"
+            return 1
         else
-            ln -s "${CONFDIR}/config/texmf" texmf
-            cd ${CONFDIR}
-            success "LaTeX configured"
+            if ln -s "${CONFDIR}/config/texmf" ~/texmf;then
+                success "LaTeX : configured"
+                return 0
+            else
+                fail "LaTex : symbolic link failure"
+                return 1
+            fi
         fi
     else
-        fail "LaTeX configuration failed. First Install latex"
-        ERR=2
+        fail "LaTeX : install pdflatex"
+        return 1
     fi
 }
 
 #Configure xmonad
 function config_xmonad() {
     highlight "\nConfiguring xmonad"
-    if hash xmonad; then
-        cd
-        if [ -d .xmonad ]; then
-            fail "Xmonad configuration failed. Delete ~/.xmonad and retry"
-            ERR=1
+
+    if hash xmonad &>/dev/null ; then
+        if [ -d ~/.xmonad ]; then
+            fail "Xmonad : Delete ~/.xmonad and retry"
+            return 1
         else
-            ln -s "${CONFDIR}/config/xmonad" .xmonad
-            cd ${CONFDIR}
-            xmonad --recompile
-            success "Xmonad configured"
+            if ln -s "${CONFDIR}/config/xmonad" ~/.xmonad && xmonad --recompile; then
+                success "Xmonad : configured"
+                return 0
+            else
+                fail "Xmond : symbolic link failure"
+                return 1
+            fi
         fi
     else
-        fail "xmonad configuration failed. Install xmonad first"
-        ERR=2
+        fail "xmonad : Install xmonad"
+        return 1
     fi
 
     config_xinitrc
 }
 
+# Remap directly
+function config_remap() {
+    highlight "\nConfiguring xmodmap"
+
+    # Map caps lock to escape
+    if dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"; then
+        success "Remap : successful"
+        return 0
+    else
+        fail "Remap : failed"
+        return 1
+    fi
+}
+
 #Solarize the terminal
 function config_solarize() {
     highlight "\nConfiguring the color scheme"
+
     if [ -e ~/.dircolors-light ]; then
-        fail "Solarize configuration failed. Delete ~/.dircolors and retry"
-        ERR=1
+        fail "Solarize : Delete ~/.dircolors and retry"
+        return 1
     else
         cp shells/dircolors.ansi-light ~/.dircolors-light
         cp shells/dircolors.ansi-dark ~/.dircolors-dark
@@ -212,48 +262,44 @@ function config_solarize() {
     fi
 }
 
-# Remap directly
-function config_remap() {
-    highlight "\nConfiguring xmodmap"
-
-    # Map caps lock to escape
-    if dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"; then
-        success "Remap successful"
-    else
-        fail "Remap failed"
-        ERR=1
-    fi
-}
-
 #This is for xinitrc
 function config_xinitrc() {
     highlight "\nConfiguring xinitrc"
-    cd
-    if [ -e .xinitrc ]; then
-        fail "Xinitrc configuration Failed. Delete ~/.xinitrc and retry"
-        ERR=1
+
+    if [ -e ~/.xinitrc ]; then
+        fail "Xinitrc : Delete ~/.xinitrc and retry"
+        return 1
     else
-        ln -s "${CONFDIR}/config/system/xinitrc" .xinitrc
-        success "Xinitrc configured"
+        if ln -s "${CONFDIR}/config/system/xinitrc" ~/.xinitrc;then
+            success "Xinitrc : configured"
+            return 0
+        else
+            fail "Xinitrc : symbolic link failure"
+            return 1
+        fi
     fi
 }
 
 # Configure sublime text
 function config_sublime() {
     highlight "\nConfiguring sublime text"
-    if hash subl; then
-        cd ~/.config
-        if [ -d sublime-text-3 ]; then
-            fail "Sublime text configuration failed. Remove ~/.config/sublime-text3 and retry."
-            ERR=1
+
+    if hash subl &> /dev/null; then
+        if [ -d ~/.config/sublime-text-3 ]; then
+            fail "Sublime text : Remove ~/.config/sublime-text3 and retry."
+            return 1
         else
-            ln -s "${CONFDIR}/config/sublime-text3" sublime-text-3
-            cd ${CONFDIR}
-            success "Sublime Text configured"
+            if ln -s "${CONFDIR}/config/sublime-text3" ~/.config/sublime-text-3;then
+                success "Sublime Text : configured"
+                return 0
+            else
+                fail "Sublime Text : symbolic link failure"
+                return 1
+            fi
         fi
     else
-        fail "Sublime text configuration failed. Install sublime text and then retry"
-        ERR=2
+        fail "Sublime text : Install sublime text and then retry"
+        return 1
     fi
 }
 
@@ -265,14 +311,17 @@ function config_ssh() {
         mkdir ~/.ssh
     fi
 
-    cd ~/.ssh
-    if [ -f config ]; then
-        fail "SSH configuration failed. Remove ~/ssh/config and retry."
-        ERR=1
+    if [ -f ~/.ssh/config ]; then
+        fail "SSH : Remove ~/ssh/config and retry."
+        return 1
     else
-        ln -s "${CONFDIR}/config/system/sshconfig" config
-        cd ${CONFDIR}
-        success "SSH configured"
+        if ln -s "${CONFDIR}/config/system/sshconfig" ~/.ssh/config;then
+            success "SSH : configured"
+            return 0
+        else
+            fail "SSH : symbolic link failure"
+            return 1
+        fi
     fi
 }
 
@@ -350,6 +399,3 @@ while [ -n "$1" ]; do
     esac
     shift
 done
-
-# return the exit status
-exit $ERR
