540c1b840ee1db711b282b1f839f4b92d958fe43
Srijan R Shetty <srijan.shetty@gmail.com>
2014-08-02 12:23:47 +0530

    configure script

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -14,19 +14,41 @@ UNDERLINE_OFF=$(tput rmul)
 
 # function to output messages on the console
 function fail() {
-        echo "[${RED} FAIL ${NORMAL}] $*" | tee $LOGFILE
+        echo "[${RED} FAIL ${NORMAL}] $*" | tee "$LOGFILE"
 }
 
 function warn() {
-        echo "[ ${YELLOW}WARN${NORMAL} ] $*" | tee $LOGFILE
+        echo "[ ${YELLOW}WARN${NORMAL} ] $*" | tee "$LOGFILE"
 }
 
 function success() {
-        echo "[${GREEN} OKAY ${NORMAL}] $*" | tee $LOGFILE
+        echo "[${GREEN} OKAY ${NORMAL}] $*" | tee "$LOGFILE"
 }
 
 function highlight() {
-        echo "${UNDERLINE_ON}$*${UNDERLINE_OFF}" | tee $LOGFILE
+       echo "${UNDERLINE_ON}$*${UNDERLINE_OFF}" | tee "$LOGFILE"
+}
+
+function configure() {
+    highlight "\nConfiguring $1"
+
+    if hash vim &> "$LOGFILE"; then
+        if [ -d $2 ]; then
+            fail "$1 : delete $2 and retry"
+            return 1
+        else
+            if ln -s "$3" "$2" ; then
+                success "$1 : configured"
+                return 0
+            else
+                fail "$1 : failed to create symlinks"
+                return 1
+            fi
+        fi
+    else
+        fail "$1: install $1"
+        return 1
+    fi
 }
 
 # This indicates that helper has been sources
