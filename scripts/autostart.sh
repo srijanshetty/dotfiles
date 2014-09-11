e4cd016d19ed2ec4b6c2841021e42ac8358c027a
Srijan R Shetty <srijan.shetty@gmail.com>
2014-09-11 17:28:49 +0530

    Check for services before autostrating

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,5 +1,21 @@
-#!/bin/sh
+#!/bin/zsh
 
-/home/srijan/Documents/GitHub/xSwipe/xSwipe.pl &
-btsync &> /dev/null
-sudo service auth start
+# Source required files
+DOT_DIR_NAME="$(dirname "$0")"
+[ -z $DOT_HELPER ] && source "${DOT_DIR_NAME}/helper.sh"
+
+if pgrep xSwipe.pl &> /dev/null; then
+    warn "xSwipe is running"
+else
+    /home/srijan/Documents/GitHub/xSwipe/xSwipe.pl &
+    success "xSwipe is now running"
+fi
+
+if pgrep btsync &> /dev/null; then
+    warn "Btsync is running"
+else
+    btsync &> /dev/null
+    success "btsync is now running"
+fi
+
+# sudo service auth start
