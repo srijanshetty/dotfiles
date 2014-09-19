5f6743b2214d569d95acec71bec310aa597b4101
Srijan R Shetty <srijan.shetty@gmail.com>
2014-09-19 20:47:56 +0530

    Refined autostart with start and stop commands

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,18 +4,36 @@
 DOT_DIR_NAME="$(dirname "$0")"
 [ -z $DOT_HELPER ] && source "${DOT_DIR_NAME}/helper.sh"
 
-if pgrep xSwipe.pl &> /dev/null; then
-    warn "xSwipe is running"
-else
-    /home/srijan/Documents/GitHub/xSwipe/xSwipe.pl &
-    success "xSwipe is now running"
-fi
+function run() {
+    if pgrep $1 &> /dev/null; then
+        warn "$1 is running"
+    else
+        if $2 &> /dev/null &; then
+            success "$1 is now running"
+        else
+            fail "$1 running failed"
+        fi
+    fi
 
-if pgrep btsync &> /dev/null; then
-    warn "Btsync is running"
-else
-    btsync &> /dev/null
-    success "btsync is now running"
-fi
+}
 
-# sudo service auth start
+function processkill() {
+    if pgrep $1 &> /dev/null; then
+        pkill $1 && success "$1 killed"
+    else
+        warn "$1 is not running"
+    fi
+}
+
+case $1 in
+    kill|stop)
+        processkill 'xSwipe.pl'
+        processkill 'btsync'
+        processkill 'firewall-auth'
+        ;;
+    *)
+        run 'xSwipe.pl' '/home/srijan/Documents/GitHub/xSwipe/xSwipe.pl'
+        run 'btsync' 'btsync'
+        run 'firewall-auth' '/home/srijan/Documents/firewall-auth'
+        ;;
+esac
