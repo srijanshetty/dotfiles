4ae2431ad0d40db98499efefc0638a1fcc1dbecd
Srijan R Shetty <srijan.shetty@gmail.com>
2014-09-19 20:59:27 +0530

    Removed some files and renamed the sh extension

--- scripts/ip.zsh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-curl ifconfig.me
--- scripts/localIP.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-IP=$(ifconfig wlan0 | grep 'inet ' | awk '{print $2}' | awk -F : '{print $2}')
-
-if [ -n "$IP"]; then
-    IP=$(ifconfig eth0 | grep 'inet ' | awk '{print $2}' | awk -F : '{print $2}')
-fi
-
-echo $IP
