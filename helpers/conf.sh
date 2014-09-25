bdc69185c09f512a108c9abdea55389aad4bbb6d
Srijan R Shetty <srijan.shetty@gmail.com>
2014-09-25 13:32:14 +0530

    Now the bin is in local/bin

--- helpers/conf.sh
+++ helpers/conf.sh
@@ -3,7 +3,7 @@
 # Location of directories
 DOTFILE_DIR=~/Documents/dotfiles
 GITHUB_DIR=~/Documents/GitHub
-LOCAL_BIN=~/Documents/local
+LOCAL_BIN=~/Documents/local/bin
 
 # directory for the logfile
 LOGFILE=/dev/null
